c196be02925acf41c8f9f7367221c549cfac0139
Sebastian-Cristian Ciuta <cloudraiders24@gmail.com>
2025-11-19 19:19:20 +0100

    feat : ajout du projet

---
+++ java/com/example/bibliotheque_quali_dev/BibliothequeQualiDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotheque_quali_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeQualiDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliothequeQualiDevApplication.class, args);
+    }
+
+}
