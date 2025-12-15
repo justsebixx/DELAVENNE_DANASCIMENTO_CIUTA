80102f0609baed08cec21cc3ecfd03160922cdb2
Sebastian-Cristian Ciuta <cloudraiders24@gmail.com>
2025-12-15 19:44:03 +0100

    chore : add backend folder

---
+++ backend/src/test/java/com/example/bibliotheque_quali_dev/BibliothequeQualiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotheque_quali_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeQualiDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
