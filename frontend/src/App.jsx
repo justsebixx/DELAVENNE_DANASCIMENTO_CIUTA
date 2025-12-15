54b136ebcb665deca41f7a1e37d37c24aa776c08
Sebastian-Cristian Ciuta <cloudraiders24@gmail.com>
2025-12-15 19:52:30 +0100

    chore : add frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import ScrollToTop from './components/ScrollToTop';
+import Home from './pages/Home';
+import Books from './pages/Books';
+import AddBook from './pages/AddBook';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import LegalNotice from './pages/LegalNotice';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import './App.css';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/add-book" element={<AddBook />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/legal" element={<LegalNotice />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header className="header">
+            <nav className="nav-left">
+                <Link to="/" className="nav-link">Accueil</Link>
+                <Link to="/books" className="nav-link">Livres</Link>
+            </nav>
+            <div className="search-container">
+                <input type="text" className="search-input" placeholder="Rechercher un livre..." />
+            </div>
+            <div className="nav-right">
+                <Link to="/login" className="nav-login">
+                    <img src="https://cdn-icons-png.flaticon.com/512/747/747376.png" alt="Login" className="login-icon" />
+                </Link>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
